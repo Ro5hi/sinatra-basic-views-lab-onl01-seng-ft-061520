74b3853c3ec2128fec28ca8dc6033683449c94d6
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-13 17:05:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
+    erb :index 
   end 
-  
-  get '/name' do 
-    "<h1>Buster's Best Bowling Alley!</h1>"
-  end 
+   
   
 end 
