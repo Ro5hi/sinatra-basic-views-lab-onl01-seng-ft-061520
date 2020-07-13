e9bfdb87abb03aef9375f2a7b0b8a796c65f2566
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-13 16:57:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
-  end
-
+  get '/' do 
+    
+  end 
 
 end
